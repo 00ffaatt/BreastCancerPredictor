514947755c60328f47db81419eaae345e89a1161
David Liao <25123938+00ffaatt@users.noreply.github.com>
2025-08-15 18:29:50 -0400

    Update csv location BreastCancerPredictor.py

--- BreastCancerPredictor.py
+++ BreastCancerPredictor.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 
-df = pd.read_csv("file://localhost/path/to/breast-cancer/Breast_cancer_dataset.csv")
+df = pd.read_csv("https://github.com/00ffaatt/BreastCancerPredictor/blob/main/Breast_cancer_dataset.csv")
 
 
 print(df)
