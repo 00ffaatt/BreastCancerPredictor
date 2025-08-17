d70358e2152999edcd18666022cde97a7c6e5f37
David Liao <david.yhliao@gmail.com>
2025-08-16 23:50:55 -0400

    added fixed random_state for training/test data split to standardize impact of preprocessing

--- BreastCancerPredictor.py
+++ BreastCancerPredictor.py
@@ -10,27 +10,32 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 url = "https://raw.githubusercontent.com/00ffaatt/BreastCancerPredictor/main/Breast_cancer_dataset.csv"
 df = pd.read_csv(url)
 
-# Pre-processing of data
+# trimming data
 df.drop(["id", "Unnamed: 32"], axis=1, inplace=True)
 df["diagnosis"] = df["diagnosis"].map({"M": 1, "B": 0})
-# Standard scaling
-stdScaler = StandardScaler()
-X = stdScaler.fit_transform(X)
-# PCA transformation
-pca = PCA()
-X_pca = pca.fit_transform(X)
-
-# *** PCA was performed after standardization of data to avoid biases in components from
-# inputs params with different scales ***
-
-print(df)
-
 # The diagnosis is what we are after as the dependent data (y)
 X = df.iloc[:, 1:31]
 y = df.iloc[:, 0]
 
+# accuracy score before preprocessing of data: 0.9473684210526315
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# PREPROCESSING DATA
+# Standard scaling
+stdScaler = StandardScaler()
+X = stdScaler.fit_transform(X)
+# PCA transformation
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+
+
+# *** PCA was performed after standardization of data to avoid biases in components from
+# inputs params with different scales ***
+
+
+# Accuracy score after preprocessing: 0.9649122807017544
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
